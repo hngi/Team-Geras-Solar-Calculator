2781fd937363a69daaf2be76b03168009a8c5f04
victory.sam-at-068278763965 <m8C+YbFkifZETjbYVbz+ntvTCkN9vCJ2OEdTrYcr5Pg=>
2019-09-24 08:22:20 +0100

    First commit

---
+++ app/src/main/java/com/hngi/team_geras_solar_calculator/ApplianceActivity.kt
@@ -0,0 +1,12 @@
+package com.hngi.team_geras_solar_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplianceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appliance)
+    }
+}
---
+++ app/src/main/java/com/hngi/team_geras_solar_calculator/ApplianceAdapter.kt
@@ -0,0 +1,21 @@
+package com.hngi.team_geras_solar_calculator
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ApplianceAdapter : RecyclerView.Adapter<ApplianceAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+}
