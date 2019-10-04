8bbbe0b106ee135e1b0aad12a4fec4944e5abf99
Adedapo Adeniji <acubedng@yahoo.com>
2019-10-04 12:50:45 +0100

    added the about app feature

--- app/src/main/java/com/hngi/team_geras_solar_calculator/MainActivity.kt
+++ app/src/main/java/com/hngi/team_geras_solar_calculator/MainActivity.kt
@@ -2,6 +2,9 @@ package com.hngi.team_geras_solar_calculator
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,7 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
+        supportActionBar?.title = "Solar Calculator"
+
+        //supportActionBar?.hide()
+
 
 
         skipBtn.setOnClickListener {
@@ -19,5 +25,28 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu to use in the action bar
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar menu items
+        when (item.itemId) {
+            R.id.how_to_use -> {
+                val intent = Intent(this, HowtoActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 }
